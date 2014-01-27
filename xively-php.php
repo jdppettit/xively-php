88d2b8e8cb62846923f39fc5e13aa3d4795b7c63
Joe Pettit <jpettit@andromeda.pettitservers.com>
2014-01-27 13:52:55 -0500

    First commit

---
+++ xively-php.php
@@ -0,0 +1,25 @@
+<?php
+	global $ENDPOINT = "/v2/feeds/";
+	global $BASEURL = "https://api.xively.com";
+	
+	function get($API_KEY,$FEED_ID,$DATASTREA_ID,
+	$w,$h,$c,$t,$l,$s,$b,$g,$scale,$min,$max,$timezone)
+	{
+		
+	}
+
+	function send($API_KEY,$FEED_ID,$data)
+	{
+
+	}
+
+	function make($API_KEY,$FEED_ID,$data)
+	{
+
+	}
+
+	function initCurl()
+	{
+		$ch = curl_init();
+	}
+?>
