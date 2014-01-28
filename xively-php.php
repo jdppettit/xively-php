4f723c2694e5e0f20329a21a4d70e21672ce2e8f
Joe Pettit <jpettit@andromeda.pettitservers.com>
2014-01-28 14:39:02 -0500

    Completed getGraph

--- xively-php.php
+++ xively-php.php
@@ -1,11 +1,37 @@
 <?php
-	global $ENDPOINT = "/v2/feeds/";
-	global $BASEURL = "https://api.xively.com";
 	
-	function get($API_KEY,$FEED_ID,$DATASTREA_ID,
-	$w,$h,$c,$t,$l,$s,$b,$g,$scale,$min,$max,$timezone)
+	define(ENDPOINT, "/v2/feeds/");
+	define(BASEURL,"https://api.xively.com");
+	
+	function getGraph($API_KEY,$FEED_ID,$DATASTREAM_ID,
+	$width,$height,$color,$title,$legend,$strokesize,
+	$axislabels,$detailedgrid,$scale,$min,$max,$timezone)
 	{
+		$graph_url = BASEURL . ENDPOINT . $FEED_ID . "/datastreams/" . $DATASTREAM_ID . ".png?";
 		
+		$args = array('width' 		 => $w,
+					  'height' 		 => $h,
+					  'color' 		 => $c,
+					  'title' 		 => $t,
+					  'legend' 		 => $l,
+					  'stroke' 		 => $s,
+					  'axislabels'   => $b,
+					  'detailedgrid' => $g,
+					  'scale' 		 => $scale,
+					  'min' 		 => $min,
+					  'max' 		 => $max,
+					  'timezone' 	 => $timzezone);
+					  
+		foreach($args as $key => $arg)
+		{
+			if(isset($arg) && $arg != "")
+			{
+				$graph_url .= $key."=".$arg;
+			}
+		}
+		
+		$html_encoded = '<img src="'.$graph_url.'" alt="'.$args['title'].'"/>';
+		return $html_encoded;
 	}
 
 	function send($API_KEY,$FEED_ID,$data)
@@ -18,8 +44,9 @@
 
 	}
 
-	function initCurl()
+	function initCurl($url)
 	{
-		$ch = curl_init();
+		$ch = curl_init($url);
+		return $ch;
 	}
 ?>
