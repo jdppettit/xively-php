e4c09530a853759c02b94cea01f6dcefac155391
silverp1 <jpettit@pettitservers.com>
2014-01-28 20:44:46 -0500

    Added sendDatapoint and createDatastream functions

--- xively-php.php
+++ xively-php.php
@@ -33,12 +33,63 @@
 		return $html_encoded;
 	}
 
-	function send($API_KEY,$FEED_ID,$data)
+	function sendDatapoint($API_KEY,$FEED_ID,$data)
 	{
+		$url = BASEURL . ENDPOINT . $FEED_ID;
+		$header = array("X-ApiKey: " . $API_KEY, "Content-length: " . strlen($data));
+	        echo $url."<br/>";
+		echo $header."<br/>";
+		var_dump($header);	
+		$ch = initCurl($url);
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		
+		$response = curl_exec($ch);
+		$error = curl_error($ch);
+
+		if($error != "")
+		{
+			$response = $error;
+			return $response;
+		}
+
+		return $response;
+		
+	}
+
+	function makeDatastream($API_KEY,$FEED_ID,$data)
+	{
+		$url = BASEURL . ENDPOINT . $FEED_ID;
+                $header = array("X-ApiKey: " . $API_KEY, "Content-length: " . strlen($data));
+
+                $ch = initCurl($url);
+
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_VERBOSE, 1);
+                curl_setopt($ch, CURLOPT_BINARYTRANSFER, TRUE);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+                $response = curl_exec($ch);
+                $error = curl_error($ch);
+
+                if($error != "")
+                {
+                        $response = $error;
+                        return $response;
+                }
+
+                return $response;
 
 	}
 
-	function make($API_KEY,$FEED_ID,$data)
+	function deleteDatastream($API_KEY,$FEED_ID,$data)
 	{
 
 	}
@@ -48,4 +99,5 @@
 		$ch = curl_init($url);
 		return $ch;
 	}
+
 ?>
